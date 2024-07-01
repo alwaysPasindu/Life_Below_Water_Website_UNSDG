92e30de136dfbbb4c630fe54293ae9a5b3819f9a
akilalochana <akilalochana7@gmail.com>
2024-07-01 09:48:21 +0530

    update js

--- profile.js
+++ profile.js
@@ -1,9 +1,20 @@
-document.getElementById("next").onclick = function () {
 
-    var enteredName = document.getElementById("name").value;
-    if (enteredName == "") {
-        alert("Please enter your name!")
+function textTo(nextButton, text, previousD1, nextD1) {
+    document.getElementById(nextButton).onclick = function () {
+
+        var enteredName = document.getElementById(text).value;
+        if (enteredName == "") {
+            alert("Please enter your name!")
+        }
+        else {
+            document.getElementById(previousD1).style.display = "none";
+            document.getElementById(nextD1).style.display = "block";
+        }
+
+
     }
+}
 
+textTo('next1', 'name', 'div1Name', 'div1Surname');
 
-}  
+textTo('next2', 'surname', 'div1Surname', 'div1Age');
