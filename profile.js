ef10de7f11f26637f4d2a2dd68dfa7b45957d88e
akilalochana <akilalochana7@gmail.com>
2024-07-01 22:29:49 +0530

    update js
    add surname,age,gender inputs and connect with js

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 
-function textTo(nextButton, text, previousD1, nextD1) {
+function textTo(nextButton, text, previousD1, nextD1, output) {
     document.getElementById(nextButton).onclick = function () {
 
         var enteredName = document.getElementById(text).value;
@@ -9,12 +9,52 @@ function textTo(nextButton, text, previousD1, nextD1) {
         else {
             document.getElementById(previousD1).style.display = "none";
             document.getElementById(nextD1).style.display = "block";
+
+
+            document.getElementById(output).innerHTML = enteredName;
+
         }
 
 
     }
 }
 
-textTo('next1', 'name', 'div1Name', 'div1Surname');
 
-textTo('next2', 'surname', 'div1Surname', 'div1Age');
+function radioButton() {
+    document.getElementById("next4").onclick = function () {
+        // Get all radio buttons with the name 'myRadio'
+        var radios = document.getElementsByName('myRadio');
+        var selectedValue = null;
+
+        // Loop through the radio buttons to find the selected one
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                selectedValue = radios[i].value;
+                break;
+            }
+        }
+
+        // Check if a value is selected and show an alert
+        if (selectedValue) {
+            document.getElementById('outputGender').innerHTML = selectedValue;
+        } else {
+            alert('No option selected');
+        }
+    }
+}
+
+
+
+
+
+
+
+textTo('next1', 'name', 'div1Name', 'div1Surname', 'outputName');
+
+textTo('next2', 'surname', 'div1Surname', 'div1Age', 'outputSurname');
+
+textTo('next3', 'age', 'div1Age', 'div1Gender', 'outputAge');
+
+radioButton();
+
+
